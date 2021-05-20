729b3261808b7a79e020008e49af877e2094baa5
Herb from Ubuntu <hewang@mail.bnu.edu.cn>
2021-05-20 10:39:59 +0800

    Add taks

---
+++ run1.sh
@@ -0,0 +1,25 @@
+python3.7 -m inference.gwpe_main train new nde \
+    --data_dir data/GW150914_sample_prior_basis/ \
+    --model_dir models/GW150914_sample_uniform_100basis_all_posterior_prior/ \
+    --basis_dir data/GW150914_sample_prior_basis/ \
+    --save_model_name model_sample_from_all_posterior.pt \
+    --save_aux_filename waveforms_supplementary_sample_from_all_posterior.hdf5 \
+    --nbins 8 \
+    --dont_sample_extrinsic_only \
+    --nsamples_target_event 500 \
+    --nsample 100000 \
+    --sampling_from posterior \
+    --num_transform_blocks 10 \
+    --nflows 15 \
+    --batch_norm \
+    --batch_size 2048 \
+    --lr 0.0002 \
+    --epochs 1000 \
+    --distance_prior_fn uniform_distance \
+    --hidden_dims 512 \
+    --truncate_basis 100 \
+    --activation elu \
+    --lr_anneal_method cosine
+# train data sampling from posterior
+# for all params dim
+# using uniform basis (truncate 100)
---
+++ run5.sh
@@ -0,0 +1,24 @@
+python3.7 -m inference.gwpe_main train new nde \
+    --data_dir data/GW150914_sample_prior_basis/ \
+    --model_dir models/GW150914_sample_uniform_100basis_extrinsic_uniform_prior/ \
+    --basis_dir data/GW150914_sample_prior_basis/ \
+    --save_model_name model.pt \
+    --save_aux_filename waveforms_supplementary.hdf5 \
+    --nbins 8 \
+    --nsamples_target_event 500 \
+    --nsample 100000 \
+    --sampling_from uniform \
+    --num_transform_blocks 10 \
+    --nflows 15 \
+    --batch_norm \
+    --batch_size 2048 \
+    --lr 0.0002 \
+    --epochs 1000 \
+    --distance_prior_fn uniform_distance \
+    --hidden_dims 512 \
+    --truncate_basis 100 \
+    --activation elu \
+    --lr_anneal_method cosine
+# train data sampling from uniform
+# for extrinsic params dim
+# using uniform basis (truncate 100)
