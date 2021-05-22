c3348452b2fe9f7181da05d7e8d98fcf70b3fb9e
Herb from Ubuntu <hewang@mail.bnu.edu.cn>
2021-05-22 22:22:43 +0800

    Add scripts for resuming (existing model)

--- run5.sh
+++ run5.sh
@@ -23,3 +23,21 @@ python3.7 -m inference.gwpe_main train new nde \
 # train data sampling from uniform
 # for extrinsic params dim
 # using uniform basis (truncate 100)
+
+## Used for resuming running
+# python3.7 -m inference.gwpe_main train existing nde \
+#     --data_dir data/GW150914_sample_prior_basis/ \
+#     --model_dir models/GW150914_sample_uniform_100basis_extrinsic_uniform_prior/ \
+#     --basis_dir data/GW150914_sample_prior_basis/ \
+#     --save_model_name model.pt \
+#     --save_aux_filename waveforms_supplementary.hdf5 \
+#     --nsamples_target_event 1000 \
+#     --nsample 100000 \
+#     --sampling_from uniform \
+#     --output_freq 10 \
+#     --batch_size 2048 \
+#     --lr 0.0002 \
+#     --epochs 2000 \
+#     --distance_prior_fn uniform_distance \
+#     --truncate_basis 100 \
+#     --lr_anneal_method cosine
