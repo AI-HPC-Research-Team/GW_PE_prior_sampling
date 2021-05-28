cb9a0e780f18892d9a58282878bc7cee583577f1
iphysresearch <hewang@mail.bnu.edu.cn>
2021-05-28 10:08:57 +0800

    Logfiles

--- run1.sh
+++ run1.sh
@@ -1,12 +1,12 @@
 python3.7 -m inference.gwpe_main train new nde \
     --data_dir data/GW150914_sample_prior_basis/ \
-    --model_dir models/GW150914_sample_uniform_100basis_all_posterior_prior/ \
+    --model_dir models/GW150914_sample_uniform_100basis_all_posterior_prior_1024hiddendims/ \
     --basis_dir data/GW150914_sample_prior_basis/ \
-    --save_model_name model_sample_from_all_posterior.pt \
-    --save_aux_filename waveforms_supplementary_sample_from_all_posterior.hdf5 \
+    --save_model_name model.pt \
+    --save_aux_filename waveforms_supplementary.hdf5 \
     --nbins 8 \
     --dont_sample_extrinsic_only \
-    --nsamples_target_event 1000 \
+    --nsamples_target_event 2000 \
     --nsample 100000 \
     --sampling_from posterior \
     --num_transform_blocks 10 \
@@ -15,31 +15,31 @@ python3.7 -m inference.gwpe_main train new nde \
     --batch_size 2048 \
     --output_freq 10 \
     --lr 0.0002 \
-    --epochs 2000 \
+    --epochs 3000 \
     --distance_prior_fn uniform_distance \
-    --hidden_dims 512 \
-    --truncate_basis 100 \
+    --hidden_dims 1024 \
+    --truncate_basis 200 \
     --activation elu \
     --lr_anneal_method cosine
 # train data sampling from posterior
 # for all params dim
 # using uniform basis (truncate 100)
 
-## Used for resuming running
-# python3.7 -m inference.gwpe_main train existing \
-#     --data_dir data/GW150914_sample_prior_basis/ \
-#     --model_dir models/GW150914_sample_uniform_100basis_all_posterior_prior/ \
-#     --basis_dir data/GW150914_sample_prior_basis/ \
-#     --save_model_name model.pt \
-#     --save_aux_filename waveforms_supplementary.hdf5 \
-#     --dont_sample_extrinsic_only \
-#     --nsamples_target_event 1000 \
-#     --nsample 100000 \
-#     --sampling_from posterior \
-#     --batch_size 2048 \
-#     --output_freq 10 \
-#     --lr 0.0002 \
-#     --epochs 2000 \
-#     --distance_prior_fn uniform_distance \
-#     --truncate_basis 100 \
-#     --lr_anneal_method cosine
+## Used for resuming running 
+python3.7 -m inference.gwpe_main train existing \
+     --data_dir data/GW150914_sample_prior_basis/ \
+     --model_dir models/GW150914_sample_uniform_100basis_all_posterior_prior/ \
+     --basis_dir data/GW150914_sample_prior_basis/ \
+     --save_model_name model.pt \
+     --save_aux_filename waveforms_supplementary.hdf5 \
+     --dont_sample_extrinsic_only \
+     --nsamples_target_event 1000 \
+     --nsample 100000 \
+     --sampling_from posterior \
+     --batch_size 2048 \
+     --output_freq 10 \
+     --lr 0.0002 \
+     --epochs 2000 \
+     --distance_prior_fn uniform_distance \
+     --truncate_basis 100 \
+     --lr_anneal_method cosine
