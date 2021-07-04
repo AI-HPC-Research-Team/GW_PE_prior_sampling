7cbfd387233f011e5f9268eec0038ac736dad21e
iphysresearch <hewang@mail.bnu.edu.cn>
2021-07-04 23:55:53 +0800

    Fix load_bilby_samples for waveform.py

--- inference/waveform.py
+++ inference/waveform.py
@@ -550,7 +550,7 @@ class WaveformDataset(object):
             df = pd.read_csv('../bilby_runs/downsampled_posterior_samples_v1.0.0/{}_downsampled_posterior_samples.dat'.format(event), sep=' ')
         except:
             df = pd.read_csv('./downsampled_posterior_samples_v1.0.0/{}_downsampled_posterior_samples.dat'.format(event), sep=' ')
-        self.bilby_samples = df.dropna()[['mass_1', 'mass_2', 'phase', 'geocent_time', 'luminosity_distance',
+        bilby_samples = df.dropna()[['mass_1', 'mass_2', 'phase', 'geocent_time', 'luminosity_distance',
                               'a_1', 'a_2', 'tilt_1', 'tilt_2', 'phi_12', 'phi_jl',
                               'theta_jn', 'psi', 'ra', 'dec']].values.astype('float64')
         # Shift the time of coalescence by the trigger time
@@ -563,7 +563,7 @@ class WaveformDataset(object):
                     'GW170104', 'GW170818', 'GW170823',
                     'GW170809', 'GW170814', 'GW170729', 
                     'GW170608',]):
-            all_bilby_samples[i*50000:(i+1)*50000] = load_bilby_samples(event)
+            all_bilby_samples[i*50000:(i+1)*50000] = self.load_bilby_samples(event)
         self.bilby_samples = all_bilby_samples
 
     def _load_posterior(self, event):
